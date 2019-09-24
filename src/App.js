cd2154b7c7acda031490cfc7b55a323cba2b84e6
Shan Fuyang <fuyang@Shans-MBP.attlocal.net>
2019-09-24 13:40:20 -0700

    Set up Navbar for each page

--- src/App.js
+++ src/App.js
@@ -7,13 +7,20 @@ import Rooms from "./pages/Rooms";
 import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Errir";
 
+import { Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
-      <Home />
-      <Rooms />
-      <SingleRoom />
-      <Error />
+      <Navbar />
+      <switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:panda" component={SingleRoom} />
+        <Route component={Error} />
+      </switch>
     </>
   );
 }
